05cb908ffce7749d21072be19f3d43039c08d790
BigBadAud99 <100069603+BigBadAud99@users.noreply.github.com>
2025-07-21 17:33:14 +0700

    Update script.js

--- list/script.js
+++ list/script.js
@@ -79,7 +79,7 @@ document.addEventListener("DOMContentLoaded", function () {
                         ballDiv.appendChild(nameElement);
 
                         const imgElement = document.createElement("img");
-                        imgElement.src = `../assets/compressed/${dexName}/${name}.webp`;
+                        imgElement.src = `../assets/compressed/${dexName}/${name}.png`;
                         imgElement.alt = name;
 
                         const imgPromise = new Promise((resolve, reject) => {
